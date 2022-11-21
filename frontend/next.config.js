d416360b4069e63ab6275bdeb67bbe1d5c801318
yamato0211 <kumayama0211@gmail.com>
2022-11-22 07:13:13 +0900

    [add] webapp pwa化

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,6 @@
-// const withPWA = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
-
 /** @type {import('next').NextConfig} */
-// const config = {
-//   pwa: {
-//     dest: 'public',
-//     runtimeCaching,
-//   },
-//   reactStrictMode: true,
-// }
 
+const withPWA = require('next-pwa')
 
-// module.exports = withPWA(config)
-
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = withPWA({
+})
