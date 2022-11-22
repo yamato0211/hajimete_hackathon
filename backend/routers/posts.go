68e2fa231bd56527292b419e108273f95fcd80d3
yamato0211 <kumayama0211@gmail.com>
2022-11-22 16:17:04 +0900

    [add] delete post

--- backend/routers/posts.go
+++ backend/routers/posts.go
@@ -12,6 +12,7 @@ import (
 func InitPostRouter(pr *gin.RouterGroup) {
 	pr.GET("", middleware, getTimeLine)
 	pr.POST("", middleware, createMyPost)
+	pr.DELETE("/:post_id", deletePost)
 }
 
 func createMyPost(c *gin.Context) {
@@ -54,4 +55,30 @@ func getTimeLine(c *gin.Context) {
 		})
 	}
 	c.JSON(http.StatusOK, p)
+}
+
+func deletePost(c *gin.Context) {
+	var (
+		userId  any
+		isExist bool
+	)
+
+	if userId, isExist = c.Get("user_id"); !isExist {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "token is invalid",
+		})
+		return
+	}
+
+	postId := c.Param("post_id")
+	err := cruds.DeletePost(postId, userId.(string))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
 }
