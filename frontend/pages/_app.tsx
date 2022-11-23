4c48276b937c1a6d63901a5f794492d447624ff9
yamato0211 <kumayama0211@gmail.com>
2022-11-23 15:44:50 +0900

    [add] userpage _app.tsx logic

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -7,14 +7,26 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import PersonIcon from '@mui/icons-material/Person';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import HomeIcon from '@mui/icons-material/Home';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import { useLayoutEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
+
+  useLayoutEffect(() => {
+    const authCheck = () => {
+      if(router.pathname !== "/Signup" && router.pathname !== "/Signin"){
+        if(localStorage.getItem("token") === null) {
+          router.push("/Signup")
+        }
+      }
+    }
+    authCheck()
+  },[])
+
   const handleLogout = () => {
     localStorage.removeItem("token")
     localStorage.removeItem("user_id")
-    router.push("/Signup")
   }
   return (
     <>
@@ -23,29 +35,38 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/index.png" />
       </Head>
-      <header style={{backgroundColor:"aquamarine", display:"flex",justifyContent:"space-between"}}>
-        <h1>Hajimete_Hackathon</h1>
-        <div>
-          <IconButton>
-            <Link href="/">
-              <HomeIcon/>
-            </Link>
-          </IconButton>
-          <IconButton>
-            <Link href="/user">
-              <PersonIcon/>
-            </Link>
-          </IconButton>
-          <IconButton>
-            <Link href="/post">
-              <PostAddIcon/>
-            </Link>
-          </IconButton>
-          <IconButton onClick={handleLogout}>
-            <LogoutIcon/>
-          </IconButton>
-        </div>
-      </header>
+      {
+        router.pathname !== "/Signup" && router.pathname !== "/Signin" ?(
+          <header style={{backgroundColor:"aquamarine", display:"flex",justifyContent:"space-between"}}>
+            <h1>Hajimete_Hackathon</h1>
+            <div>
+              <IconButton>
+                <Link href="/">
+                  <HomeIcon/>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="/user">
+                  <PersonIcon/>
+                </Link>
+              </IconButton>
+              <IconButton>
+                <Link href="/post">
+                  <PostAddIcon/>
+                </Link>
+              </IconButton>
+              <IconButton onClick={handleLogout}>
+                <Link href="/Signup">
+                  <LogoutIcon/>
+                </Link>
+              </IconButton>
+            </div>
+          </header>
+        ) : (
+          <></>
+        )
+      }
+
       <Component {...pageProps} />
     </>
   )
