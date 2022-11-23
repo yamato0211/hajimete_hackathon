fcf0b6a30eba6b7a61257f5f1c6beee76ecd495a
yamato0211 <kumayama0211@gmail.com>
2022-11-23 15:45:20 +0900

    [add] userpage style and loginlogic

--- frontend/pages/user/index.tsx
+++ frontend/pages/user/index.tsx
@@ -1,5 +1,7 @@
 export default function User() {
     return (
-        <h1>User Page</h1>
+        <div style={{width:"100%",height:"100%",backgroundColor:"aquamarine"}}>
+            <h1>User Page</h1>
+        </div>
     )
 }
